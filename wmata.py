c1da1610b1751c417e43244e2cecfd8abf9c1dac
Jeremy Carbaugh <jcarbaugh@gmail.com>
2014-04-14 16:03:27 -0400

    handle pluralization of minutes

--- wmata.py
+++ wmata.py
@@ -26,7 +26,8 @@ class MetroCommand(Command):
             elif train['Min'] == 'ARR':
                 pred = "%s is arriving." % (train['DestinationName'],)
             else:
-                pred = "%s in %s minutes." % (train['DestinationName'], train['Min'])
+                unit = 'minute' if train['Min'] == '1' else 'minutes'
+                pred = "%s in %s %s." % (train['DestinationName'], train['Min'], unit)
             trains.append(pred)
 
         return " ".join(trains) if trains else "No predictions available."
